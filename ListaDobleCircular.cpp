7bd75e6a07bf481b20f40e934813d12038123eda
iFrank22 <ftapia@fi.uba.ar>
2022-10-08 15:57:24 -0300

    actualizacion

--- ListaDobleCircular.cpp
+++ ListaDobleCircular.cpp
@@ -4,47 +4,50 @@ using namespace std;
 
 template <class T> class Lista; //para usar el friend
 
-template <class T>
-class Nodo {
+
+template <class T> class Nodo {
     friend class Lista<T>;
     private:
     T _dato;
-    Nodo<T> *_sig;
-    Nodo<T> *_ant;
+    Nodo<T> *_siguiente;
+    Nodo<T> *_anterior;
     public:
-    Nodo(T dato, Nodo<T> *sig=0, Nodo<T> *ant=0) {
-        _dato = dato;
-        _sig = sig;
-        _ant = ant;
-    };
+    Nodo(T dato);
 };
 
+template <class T> Nodo<T>::Nodo(T dato) {
+    _dato = dato;
+    _siguiente = NULL;
+    _anterior = NULL;
+
+}
 
 
 
-template <class T>
-class Lista {
+template <class T> class Lista {
     private:
     Nodo<T> *primero;
-    Nodo<T> *ultimo;
     Nodo<T> *cursor;
     int largo;
     public:
+    //constructor
     Lista();
+    //destructor
     ~Lista();
-    //agrega el elemento pasado al final de la lista
+    //agrega el elemento pasado al final
     void add(T elemento);
-    //inerta el elemento pasado en una posicion especifica
+    //inserta el elemento pasado en una posicion especifica
 	void add(T elemento, int posicion);
-    //saca el primero elemento que sea igual al pasado?
+    //saca el primero elemento que sea igual al pasado? falta implementar
     void remove(T elemento);
     //devuelve el largo de la lista
     int getLargo();
-    //borra al elemento en la posicion pasada
-
-    bool vacia();
+    //verifica si la lista esta vacia
+    bool estaVacia();
     //muestra la lista
     void emitir();
+    //mueve el cursor a la primera posicion
+    void inicializarCursor();
     //avanza el cursor un lugar
     void avanzarCursor();
     //retrocede el cursor un lugar
@@ -54,132 +57,118 @@ class Lista {
 };
 
 
-template <class T>
-Lista <T>::Lista() {
-    primero=0;
-    ultimo=0;
-    cursor=0;
+template <class T> Lista <T>::Lista() {
+    primero=NULL;
+    cursor=NULL;
     largo=0;
-
 }
 
 
-template <class T>
-Lista<T>::~Lista() {
+template <class T> Lista<T>::~Lista() {
     if(primero) {
-        if (primero->_sig == primero) {
+        if (primero->_siguiente == primero) {
             delete primero;
         }
         else {   
-            Nodo <T> *aux1, *aux2;
-            aux1=primero->_sig;
-            primero->_sig =0;
-            while(aux1!=0) {
-                aux2=aux1;
-                aux1=aux1->_sig;
-                delete aux2;
+            Nodo <T> *nodoActual, *nodoAux;
+            nodoActual=primero->_siguiente;
+            primero->_siguiente =0;
+            while(nodoActual!=0) {
+                nodoAux=nodoActual;
+                nodoActual=nodoActual->_siguiente;
+                delete nodoAux;
             }
         }
     }
 }
 
 
-template<class T> 
-void Lista<T>::add(T elemento) {
+template <class T> void Lista<T>::add(T elemento) {
 	add(elemento, largo);
 }
 
 
-template <class T>
-void Lista<T>::add(T elemento, int posicion) {
-    
-    if(posicion < 0 || posicion > largo) {
-        return;
+template <class T> void Lista<T>::add(T elemento, int posicion) {
+    if(posicion < 0 || posicion > largo) {//validar la posicion ingresda
+        throw invalid_argument("posicion fuera de rango");
     }  
-    Nodo<T> *nuevo=new Nodo<T>(elemento);
-    if(vacia()) {
-        primero = nuevo;
-        ultimo = nuevo;
-        nuevo->_sig=nuevo;
-        nuevo->_ant=nuevo;
+    Nodo<T> *nuevoNodo=new Nodo<T>(elemento);
+    if(estaVacia()) {
+        nuevoNodo->_siguiente=nuevoNodo;
+        nuevoNodo->_anterior=nuevoNodo;
+        primero = nuevoNodo;
     }
     else {
-        Nodo<T> *aux1, *aux2;
-        aux1 = primero;
-        aux2 = ultimo;
+        Nodo<T> *nodoActual;
+        nodoActual = primero;
         int cont = 0;
-        while(cont < posicion) {
-            aux2 = aux1;
-            aux1 = aux1->_sig;
+        while(cont < posicion) {         
+            nodoActual = nodoActual->_siguiente;
             cont++;
         }
-        nuevo->_sig=aux1;
-        nuevo->_ant=aux2;
-        aux2->_sig=nuevo;
-        aux1->_ant=nuevo;
+        nuevoNodo->_siguiente=nodoActual;
+        nuevoNodo->_anterior=nodoActual->_anterior;
+        nodoActual->_anterior->_siguiente=nuevoNodo;
+        nodoActual->_anterior=nuevoNodo;
 
         if(posicion==0) {
-            primero = nuevo;
-        }
-        if(posicion==largo) {
-            ultimo = nuevo;
+            primero = nuevoNodo;
         }
     }
     largo++;
+    inicializarCursor();
 }
 
 
-template <class T>
-void Lista<T>::avanzarCursor() {
-    if(cursor==0) {
-        cursor=primero;
-    }
-    else {
-        cursor=cursor->_sig;
-    }
+template <class T> void Lista<T>::inicializarCursor(){
+    cursor=primero;
 }
 
 
-template <class T>
-void Lista<T>::retrocederCursor() {
-    if(cursor==0) {
-        cursor=ultimo;
-    }
-    else {
-        cursor=cursor->_ant;
+template <class T> void Lista<T>::avanzarCursor() {
+    if(!estaVacia()) {
+        cursor=cursor->_siguiente;
     }
 }
 
 
-template <class T>
-T Lista<T>::getCursor() {
+template <class T> void Lista<T>::retrocederCursor() {
+    if(!estaVacia()) {
+        cursor=cursor->_anterior;
+    }
+}
+
+
+template <class T> T Lista<T>::getCursor() {
+    if(cursor==NULL) {
+        throw invalid_argument("cursor invalido para una lista vacia");
+    }  
     return cursor->_dato;
 }
 
 
-template <class T>
-void Lista<T>::emitir() {  
+template <class T> void Lista<T>::emitir() {  
     Nodo<T> *aux=primero;
     cout<<"[";
-    do {
-        cout<<aux->_dato;
-        if (aux!=ultimo) {
-            cout<<',';
-        }   
-        aux=aux->_sig;
-    } while(aux!=primero);
+    if(!estaVacia()) {
+        do {
+            cout<<aux->_dato;
+            if (aux!=primero->_anterior) {
+                cout<<',';
+            }   
+            aux=aux->_siguiente;
+        } while(aux!=primero);
+    }
     cout<<"]"<<endl;
 }
 
 
-template <class T>
-bool Lista <T>::vacia() {
-    return(primero==0);
+template <class T> bool Lista <T>::estaVacia() {
+    return(primero==NULL);
 }
 
 
-template <class T>
-int Lista<T>::getLargo() {
+template <class T> int Lista<T>::getLargo() {
     return largo;
 }
 
@@ -190,14 +179,14 @@ int main() {
 
     c1.add('a');
     c1.add('b');
+    c1.add('u', 0);
     c1.add('c');
-    c1.add('0');
     c1.emitir();
+    
 
-    for (int i=1; i<=1; i++) {
+    for (int i=1; i<=2; i++) {
         c1.avanzarCursor();
     }
-
     for (int i=1; i<=0; i++) {
         c1.retrocederCursor();
     }
