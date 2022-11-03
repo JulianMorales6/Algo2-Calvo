ebbe5b30c85f56d73b16d91b01b01290f2cf88e9
zzz <zpazos@fi.uba.ar>
2022-11-03 10:57:49 -0300

    Lectura de csv de dificultades

---
+++ Archivos/Archivos.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#define ARCHIVO_DIFICULTADES "data.csv"
+#define DIFICULTAD 0
+#define ALTURA_TABLERO 1
+#define ANCHO_TABLERO 2
+#define PROFUND_TABLERO 3
+#define VECINAS_P_NACER 4
+#define MIN_SEGUIR_VIVA 5
+#define MAX_SEGUIR_VIVA 6
+
+using namespace std;
+
+struct dificultad
+{
+    string dificultad,
+        alturaTablero,
+        anchoTablero,
+        profundidadTablero,
+        vecinasParaNacer,
+        minParaSeguirViva,
+        maxParaSeguirViva;
+};
+
+
+int main()
+{
+    dificultad lista_dificultades[2];
+    ifstream archivo(ARCHIVO_DIFICULTADES);
+    string linea;
+    char separador = ',';
+    getline(archivo,linea);
+    while(getline(archivo,linea)) {
+        dificultad nuevaDificultad;
+        stringstream stream(linea);
+        // string dificultad, alturaTablero, anchoTablero, profundidadTablero, vecinasParaNacer, minParaSeguirViva, maxParaSeguirViva;
+        getline(stream, nuevaDificultad.dificultad, separador);
+        getline(stream, nuevaDificultad.alturaTablero, separador);
+        getline(stream, nuevaDificultad.anchoTablero, separador);
+        getline(stream, nuevaDificultad.profundidadTablero, separador);
+        getline(stream, nuevaDificultad.vecinasParaNacer, separador);
+        getline(stream, nuevaDificultad.minParaSeguirViva, separador);
+        getline(stream, nuevaDificultad.maxParaSeguirViva, separador);
+
+        lista_dificultades[0] = nuevaDificultad;
+
+        cout<<"Dificultad: "<<nuevaDificultad.dificultad<<endl<<"Altura del tablero: "<<nuevaDificultad.alturaTablero<<endl<<"Ancho del tablero: "<<nuevaDificultad.anchoTablero<<endl<<"Profundidad del tablero: "<<nuevaDificultad.profundidadTablero<<endl<<"Vecinas vivas para que nazca una celula: "<<nuevaDificultad.vecinasParaNacer<<endl<<"Limite inferior de vecinas vivas para que siga viva una celula: "<<nuevaDificultad.minParaSeguirViva<<endl<<"Limite superior de vecinas vivas para que siga viva una celula"<<nuevaDificultad.maxParaSeguirViva<<endl<<endl;
+
+    }
+    archivo.close();
+}
