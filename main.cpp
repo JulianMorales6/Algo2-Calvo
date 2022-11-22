0dcf03b1a46fe87b7a9da7dc07fb2117a6de4ce7
zzz <zpazos@fi.uba.ar>
2022-11-21 23:45:06 -0300

    Se agrega al main funcion que elimina los bmp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 
 #include "JuegoDeLaVida.cpp"
+#include <string>
+#include <dirent.h>
+#include <iostream>
+
+void vaciarCarpetaImagenes() {
+
+    DIR *directorio = opendir("imagenes/");
+    struct dirent *archivo;
+    char rutaCompleta[64] = "imagenes/";
+
+    if(directorio != NULL) {
+        while((archivo = readdir(directorio)) != NULL) {
+            char ruta[64] = "imagenes/";
+            strcat(ruta, archivo->d_name);
+            cout<<ruta<<endl;
+            remove(ruta);
+        }
+    }
+}
 
 int main() {
-
+    vaciarCarpetaImagenes();
     JuegoDeLaVida * juego = new JuegoDeLaVida;
     juego->inicializarJuegoDeLaVida();
     juego->interaccionesUsuario();
