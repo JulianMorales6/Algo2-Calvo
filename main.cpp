52cf568f07d4323189997a43a4ec94fe661b23ec
zzz <zpazos@fi.uba.ar>
2022-11-21 23:53:55 -0300

    Habian quedado lineas de mas del ultimo commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 
 #include "JuegoDeLaVida.cpp"
-#include <string>
 #include <dirent.h>
 #include <iostream>
 
@@ -8,13 +7,11 @@ void vaciarCarpetaImagenes() {
 
     DIR *directorio = opendir("imagenes/");
     struct dirent *archivo;
-    char rutaCompleta[64] = "imagenes/";
 
     if(directorio != NULL) {
         while((archivo = readdir(directorio)) != NULL) {
             char ruta[64] = "imagenes/";
             strcat(ruta, archivo->d_name);
-            cout<<ruta<<endl;
             remove(ruta);
         }
     }
